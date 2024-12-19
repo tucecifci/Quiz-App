40de5bcd898f995a6ab4df5561c9f5e2b40fdccf
tucecifci <tucecifcii@gmail.com>
2024-12-19 12:47:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Question from './Question'
+
+function App() {
+
+  return (
+    <>
+    
+        <Question />
+    </>
+  )
+}
+
+export default App
